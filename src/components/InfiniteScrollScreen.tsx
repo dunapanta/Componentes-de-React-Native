9ad051a6c023f7c2ace3f7e6b77133b834578086
Daniel <dunapanta2@gmail.com>
2021-05-27 20:33:12 -0500

    Infinity Scroll Image

--- src/components/InfiniteScrollScreen.tsx
+++ src/components/InfiniteScrollScreen.tsx
@@ -1,12 +1,17 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, Image, StyleSheet, Text, View} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import {Header} from './Header';
 
 export const InfiniteScrollScreen = () => {
   const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5, 6]);
   const renderItem = (item: number) => {
-    return <Text style={styles.textItem}>{item}</Text>;
+    return (
+      <Image
+        source={{uri: `https://picsum.photos/id/${item}/500/400`}}
+        style={{width: '100%', height: 400}}
+      />
+    );
   };
 
   const loadMore = () => {
@@ -14,12 +19,13 @@ export const InfiniteScrollScreen = () => {
     for (let i = 0; i < 6; i++) {
       newArray[i] = numbers.length + i;
     }
-
-    setNumbers([...numbers, ...newArray]);
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1000);
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: 'blue'}}>
+    <View style={{flex: 1, backgroundColor: '3ff7733'}}>
       <FlatList
         data={numbers}
         keyExtractor={item => item.toString()}
@@ -29,6 +35,17 @@ export const InfiniteScrollScreen = () => {
         //Infinite Scroll
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
+        //Para Loading
+        ListFooterComponent={() => (
+          <View
+            style={{
+              height: 150,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <ActivityIndicator size={30} color="#225588" />
+          </View>
+        )}
       />
     </View>
   );
